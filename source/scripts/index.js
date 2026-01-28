ec9e760286b011111d1aa45f128cf38739776d6c
Александра Корбакова <trukhacheva.a.s@gmail.com>
2026-01-28 15:30:41 +0300

    Добавляет стили для главной страницы (#16)
    * Добавлены стили для шапки сайта:
    Сделано выпадающее меню.
    Добавлены стили для планшетной и десктопной версий.
    Добавлены стили при наведении и нажатии.
    
    * Добавляет стили для блока hero (mobile, tablet)
    
    * Добавляет стили для блока hero (desktop) + корректирует отступы для mobile и tablet + изображение для mobile
    
    * Добавляет изображение кота в блок hero (desktop)
    
    * Добавляет стили для блока с программами
    
    * Добавляет стили для блока "Как это работает"
    
    * Добавляет стили для блока example (mobile)
    
    * Добавляет стили для блока example (tablet, desktop)
    
    * Добавляет стили для блока с контактами (mobile)
    
    * Исправляет меню при отключенном js
    
    * Добавляет стили для блока с контактами (tablet)
    
    * Добавляет стили для блока с контактами (desktop)
    
    * Корректирует стили для блока example (desktop)
    
    * Корректирует градиент и блок с параметрами  в exmple (desktop)
    
    * Добавляет стили для футера (mobile)
    
    * Добавляет стили для футера (tablet)
    
    * Добавляет стили для футера (desktop)
    
    * Добавляет стили при наведении/нажатии для главной страницы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,22 @@
 /* в этот файл добавляет скрипты*/
+const navToggle = document.querySelector('.main-header__toggle');
+const mainNav = document.querySelector('.main-navigation');
+
+navToggle.classList.remove('main-header__toggle--nojs');
+mainNav.classList.remove('main-navigation--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (mainNav.classList.contains('main-navigation--closed')) {
+    mainNav.classList.remove('main-navigation--closed');
+    mainNav.classList.add('main-navigation--opened');
+
+    navToggle.classList.remove('main-header__toggle--closed');
+    navToggle.classList.add('main-header__toggle--opened');
+  } else {
+    mainNav.classList.remove('main-navigation--opened');
+    mainNav.classList.add('main-navigation--closed');
+
+    navToggle.classList.remove('main-header__toggle--opened');
+    navToggle.classList.add('main-header__toggle--closed');
+  }
+});
